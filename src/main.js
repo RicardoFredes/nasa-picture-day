ed9ec138fec631b2de28bb9dbf8908142c46daa6
fredes <ricardofredes@gmail.com>
2020-10-26 15:38:04 -0300

    refactor: change fetch api strategy

--- src/main.js
+++ src/main.js
@@ -1,73 +1,62 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const NASA_API_KEY = "Owy40anZTbWVb0Hbe3fYEkv6OqG2qdLYyQc9y7Th";
-  const NASA_API = `https://api.nasa.gov/planetary/apod?api_key=${NASA_API_KEY}`;
+document.addEventListener('DOMContentLoaded', () => {
+  const NASA_API_KEY = 'Owy40anZTbWVb0Hbe3fYEkv6OqG2qdLYyQc9y7Th'
+  const NASA_API = `https://api.nasa.gov/planetary/apod?api_key=${NASA_API_KEY}`
+  const WEEK_DAYS = 7
+  const ONE_HOUR_IN_MS = 1000 * 60
+  const ONE_DAY_IN_MS = ONE_HOUR_IN_MS * 60 * 24
+  const WEEK_IN_MS = WEEK_DAYS * ONE_DAY_IN_MS
+  const SECURITY_TIMEZONE = 3
 
-  const image404 = "404.jpg";
-  const errorResponse = { error: true, url: image404, media_type: "image" };
+  const brand = document.getElementById('brand')
+  const nextButton = document.getElementById('next-button')
+  const previousButton = document.getElementById('previous-button')
+  const modal = document.getElementById('modal')
 
-  const WEEK_DAYS = 7;
-  const ONE_HOUR_INS_MS = 1000 * 60;
-  const ONE_DAY_IN_MS = ONE_HOUR_INS_MS * 60 * 24;
-  const SECURITY_TIMEZONE = 3;
+  let offsetWeek = 0
 
-  const brand = document.getElementById("brand");
-  const nextButton = document.getElementById("next-button");
-  const previousButton = document.getElementById("previous-button");
-  const modal = document.getElementById("modal");
+  const fetchWeekPictures = async () => {
+    const url = `${NASA_API}${getDataWeekParams(offsetWeek)}`
+    return fetch(url).then(r => (r.ok ? r.json() : []))
+  }
 
-  let offsetWeek = 0;
-  let currentDateInMS;
-  const loadCurrentDateInMS = () => {
-    const date = new Date();
-    const timeInMS = date.getTime();
-    const weeksInMS = offsetWeek * WEEK_DAYS * ONE_DAY_IN_MS;
-    const timeZoneOffsetInMS =
-      (date.getTimezoneOffset() + SECURITY_TIMEZONE) * ONE_HOUR_INS_MS;
-    currentDateInMS = timeInMS - timeZoneOffsetInMS - weeksInMS;
-  };
+  const getDataWeekParams = (offset = 0) => {
+    const timezoneInMS = (new Date().getTimezoneOffset() + SECURITY_TIMEZONE) * ONE_HOUR_IN_MS
+    const currentDateInMS = new Date().getTime() - timezoneInMS - offset * WEEK_IN_MS
+    const lastWeekInMS = currentDateInMS - WEEK_IN_MS + ONE_DAY_IN_MS
+    const startDate = getFormatDate(lastWeekInMS)
+    const endDate = getFormatDate(currentDateInMS)
+    return `&start_date=${startDate}&end_date=${endDate}`
+  }
 
-  const fetchNasaData = async (offset) => {
-    const url = `${NASA_API}${getDateParam(offset)}`;
-    return fetch(url).then((r) => (r.ok ? r.json() : errorResponse));
-  };
+  const getFormatDate = dateInMS => new Date(dateInMS).toISOString().replace(/T.*/, '')
 
-  const getDateParam = (offset = 0) => {
-    const dateInMS = currentDateInMS - ONE_DAY_IN_MS * offset;
-    const date = new Date(dateInMS).toISOString().replace(/T.*/, "");
-    return `&date=${date}`;
-  };
+  const addMediaDay = (id, data) => {
+    if (!data) return null
+    const parentElement = document.getElementById(id)
+    const { media_type, url, error } = data || {}
+    const mediaElement = media_type === 'image' ? createImageDayElement(url) : createVideoDayElement(url)
+    mediaElement.onload = () => parentElement.classList.remove('hide')
+    parentElement.append(mediaElement)
+    if (!error) parentElement.onclick = () => showModal(data, mediaElement.outerHTML)
+  }
 
-  const addMediaDay = ({ id, data }) => {
-    const parentElement = document.getElementById(id);
-    const { media_type, url, error } = data;
-    const mediaElement =
-      media_type === "image"
-        ? createImageDayElement(url)
-        : createVideoDayElement(url);
-    mediaElement.onload = () => parentElement.classList.remove("hide");
-    parentElement.append(mediaElement);
-    if (!error)
-      parentElement.onclick = () => showModal(data, mediaElement.outerHTML);
-  };
+  const createImageDayElement = url => {
+    const img = document.createElement('img')
+    img.src = url
+    return img
+  }
 
-  const createImageDayElement = (url) => {
-    const img = document.createElement("img");
-    img.src = url;
-    return img;
-  };
-
-  const createVideoDayElement = (url) => {
-    const embed = document.createElement("embed");
-    const youtubeParams =
-      "?autoplay=0&mute=1&controls=1&showinfo=0&rel=0&autohide=1";
-    embed.src = `${url}${youtubeParams}`;
-    return embed;
-  };
+  const createVideoDayElement = url => {
+    const embed = document.createElement('embed')
+    const youtubeParams = '?autoplay=0&mute=1&controls=1&showinfo=0&rel=0&autohide=1'
+    embed.src = `${url}${youtubeParams}`
+    return embed
+  }
 
   const hideModal = () => {
-    modal.classList.add("hide");
-    modal.innerHTML = "";
-  };
+    modal.classList.add('hide')
+    modal.innerHTML = ''
+  }
 
   const showModal = (data, htmlString) => {
     const template = `
@@ -78,70 +67,70 @@ document.addEventListener("DOMContentLoaded", () => {
             <p>${data.explanation}</p>
         </div>
         <a href="${data.hdurl}" title="Open large size image" target="_blank" class="media">${htmlString}</a>
-        `;
-    modal.innerHTML = template;
-    modal.classList.remove("hide");
-    pushHistory();
-  };
+        `
+    modal.innerHTML = template
+    modal.classList.remove('hide')
+    pushHistory()
+  }
 
-  const loadImages = async (day = 1) => {
-    if (day > WEEK_DAYS) return;
-    return fetchNasaData(day - 1).then((data) => {
-      addMediaDay({ id: `day${day}`, data });
-      return loadImages(day + 1);
-    });
-  };
+  const loadImages = async () => {
+    return fetchWeekPictures().then(result => {
+      const max = result.length
+      for (let i = 0; i < max; i++) {
+        addMediaDay(`day${max - i}`, result[i])
+      }
+    })
+  }
 
   const clearImages = (day = 1) => {
-    if (day > WEEK_DAYS) return;
-    const dayElement = document.getElementById(`day${day}`);
-    dayElement.classList.add("hide");
-    dayElement.innerHTML = "";
-    clearImages(day + 1);
-  };
+    if (day > WEEK_DAYS) return
+    const dayElement = document.getElementById(`day${day}`)
+    dayElement.classList.add('hide')
+    dayElement.innerHTML = ''
+    clearImages(day + 1)
+  }
 
-  previousButton.onclick = () => render(offsetWeek + 1);
+  previousButton.onclick = () => render(offsetWeek + 1)
 
-  nextButton.onclick = () => render(offsetWeek - 1);
+  nextButton.onclick = () => render(offsetWeek - 1)
 
-  brand.onclick = () => offsetWeek && render(0);
+  brand.onclick = () => offsetWeek && render(0)
 
   const pushHistory = () => {
-    const page = offsetWeek;
-    const status = { page };
-    const url = page ? `?page=${page}` : "?";
-    history.pushState(status, null, url);
-  };
+    const page = offsetWeek
+    const status = { page }
+    const url = page ? `?page=${page}` : '?'
+    history.pushState(status, null, url)
+  }
 
   window.onpopstate = () => {
-    const page = getPage();
-    if (page !== offsetWeek) render(page, { notUpdateHistory: true });
-    else hideModal();
-  };
+    const page = getPage()
+    if (page !== offsetWeek) render(page, { notUpdateHistory: true })
+    else hideModal()
+  }
 
   const updateNavigation = () => {
-    if (offsetWeek === 0) nextButton.classList.add("hide");
-    else nextButton.classList.remove("hide");
-  };
+    if (offsetWeek === 0) nextButton.classList.add('hide')
+    else nextButton.classList.remove('hide')
+  }
 
   const getPage = () => {
-    const page = location.search.replace(/.*page=(\d)/, "$1");
-    return Number(page) || 0;
-  };
+    const page = location.search.replace(/.*page=(\d)/, '$1')
+    return Number(page) || 0
+  }
 
   const render = (page, options = {}) => {
-    if (typeof page === "number") offsetWeek = page;
-    hideModal();
-    clearImages();
-    loadCurrentDateInMS();
-    loadImages();
-    updateNavigation();
-    if (!options.notUpdateHistory) pushHistory();
-  };
+    if (typeof page === 'number') offsetWeek = page
+    hideModal()
+    clearImages()
+    loadImages()
+    updateNavigation()
+    if (!options.notUpdateHistory) pushHistory()
+  }
 
   const initialize = () => {
-    render(getPage(), { notUpdateHistory: true });
-  };
+    render(getPage(), { notUpdateHistory: true })
+  }
 
-  initialize();
-});
+  initialize()
+})
